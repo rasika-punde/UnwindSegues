622e60044c4175b1f231c73be121c5c07d484610
Rasika Punde <RasikaP@Rasikas-Mac-Book-Pro.local>
2018-06-23 20:27:29 +0530

    Minor changes

--- UnwindSegues/FirstViewController.swift
+++ UnwindSegues/FirstViewController.swift
@@ -15,12 +15,6 @@ class FirstViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     @IBAction func btnGoToSecond(_ sender: Any) {
         performSegue(withIdentifier: "goToSecond", sender: nil)
@@ -28,14 +22,5 @@ class FirstViewController: UIViewController {
     
     @IBAction func unwindToVC1(segue:UIStoryboardSegue) { }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- UnwindSegues/ThirdViewController.swift
+++ UnwindSegues/ThirdViewController.swift
@@ -16,23 +16,8 @@ class ThirdViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     @IBAction func btnGoToFirst(_ sender: Any) {
         performSegue(withIdentifier: "unwindToVC1", sender: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
